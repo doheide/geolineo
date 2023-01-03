af8c699d34961f130d6b809c651bc4b3bc020b21
Dominik Heide <dominik.heide@50hertz.com>
2023-01-03 23:53:39 +0100

    Defined config and secrets

---
+++ src/conf.rs
@@ -0,0 +1,13 @@
+use conf_from_env::{SetStructFromEnv, ConfigAndSecrets};
+
+#[derive(SetStructFromEnv, Clone, Debug)]
+pub struct Config {
+    pub rust_log: String,
+    pub test_cases_subdirectory: String,
+}
+#[derive(SetStructFromEnv, Clone, Debug)]
+pub struct Secrets {
+
+}
+
+pub type CoSe = ConfigAndSecrets<Config, Secrets>;
